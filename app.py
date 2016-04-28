14b2d4486cceb524b8506a58fe0822faebd37e70
Will Schumacher <meintodengel@gmail.com>
2016-04-28 16:26:17 -0700

    remove unnecessary route

--- app.py
+++ app.py
@@ -6,10 +6,6 @@ app = Flask(__name__)
 
 text = 'Good morning!'
 
-@app.route('/')
-def index():
-    return json.dumps(get_score(text))
-
 @app.route('/', methods = ['POST'])
 def sent():
     return json.dumps([get_score(tweet['text']) for tweet in request.json])
